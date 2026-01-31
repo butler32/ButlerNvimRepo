be85af68adbe5afdd445a706f63d82913e17ea5f
butler <vladimir.dvoreckij01@gmail.com>
2026-01-31 16:51:59 +0300

    Configure Neo-tree with smart h/l navigation

--- lua/myconfig/plugins/neotree.lua
+++ lua/myconfig/plugins/neotree.lua
@@ -7,6 +7,26 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
+        require("neo-tree").setup({
+            window = {
+                mappings = {
+                    ["l"] = "open",
+                    ["h"] = function(state)
+                        local node = state.tree:get_node()
+                        if node.type == 'directory' and node:is_expanded() then
+                            require('neo-tree.sources.filesystem.commands').toggle_node(state)
+                        else
+                            require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
+                        end
+                    end,
+                },
+            },
+            filesystem = {
+                follow_current_file = { enabled = true },
+                use_libuv_file_watcher = true,
+            },
+        })
+
         vim.keymap.set("n", "<leader>e", ":Neotree toggle reveal left<CR>", { desc = "Toggle Explorer" })
     end,
 }
